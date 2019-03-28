656f87c0413e7c9eea5a948f7c8f9840065f8645
Alba <alba.ongaro@jobtome.com>
2019-03-28 18:31:33 +0100

    graphics common translated to class

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,3 +1,10 @@
 import "./scss/styles.scss";
+import GameManager from "./js/GameManager";
 
 let socket = io.connect('http://localhost:4000');
+
+const game = new GameManager({
+    context: document.getElementById('gameCanvas').getContext('2d')
+});
+
+game.colorText("right", "TradeWinds", "green", "Hello world", 50, 20);

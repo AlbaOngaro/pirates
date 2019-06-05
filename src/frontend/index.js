7914f440664abd78076c14483e785d948a66f2ba
Alba <alba.ongaro@jobtome.com>
2019-06-05 15:14:25 +0200

    refactor

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,8 +1,9 @@
 import "./scss/styles.scss";
 import GC from "./js/GraphicsCommon";
 
-let socket = io.connect('http://localhost:4000');
-
+const socket = io.connect('http://localhost:4000');
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+
+
 
